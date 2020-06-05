fc963d31b3e03b640e07bc2cb5cc83a90423f046
OmiGitProjects <onkarnardekar2407@gmail.com>
2020-06-06 00:42:12 +0530

    Fixed Admin Panel Name

--- todo/urls.py
+++ todo/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'TODO APP'                    # default: "Django Administration"
+admin.site.index_title = 'TODO Admin Panel'                 # default: "Site administration"
+admin.site.site_title = 'Welcome to TODO APP Admin Panel' # default: "Django site admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
